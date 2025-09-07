db4e6ec436b9317b551a60ea47bcf2aa84f9087d
Jeff Gutierrez <jeffgutierrez0524@gmail.com>
2025-09-07 14:15:11 -0400

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Jeff Gutierrez on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
